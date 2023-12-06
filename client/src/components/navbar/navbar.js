f006d2545831934bfbd7563b4689a1062b81b06f
CezarBaluta <cezar.baluta@yahoo.com>
2023-12-06 23:32:44 +0200

    logout implementation

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -9,11 +9,16 @@ const NavBar = () => {
   const [showRegister, setShowRegister] = useState(false);
   const [showLogin, setShowLogin] = useState(false);
   const [showPoll, setShowPoll] = useState(false);
-
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const handlePollShow = () => setShowPoll(true);
   const handlePollClose = () => setShowPoll(false);
 
 
+  const handleLogout = () => {
+
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
+  };  
 
   const handleLoginShow = () => setShowLogin(true);
   const handleLoginClose = () => setShowLogin(false);
@@ -35,6 +40,8 @@ const NavBar = () => {
             <Nav.Link onClick={handleLoginShow}>Login</Nav.Link>
             <Nav.Link onClick={handleRegisterShow}>Register</Nav.Link>
             <Nav.Link onClick={handlePollShow}>Add poll</Nav.Link>
+            <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+
           </Nav>
         </Navbar.Collapse>
       </Navbar>
