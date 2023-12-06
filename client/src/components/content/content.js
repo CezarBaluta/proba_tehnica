3e1b65b14dc9fc56b7f0a5576e44775bb206ed8c
CezarBaluta <cezar.baluta@yahoo.com>
2023-12-06 19:47:51 +0200

    content component implementation

--- client/src/components/content/content.js
+++ client/src/components/content/content.js
@@ -1,23 +1,63 @@
 import React from 'react';
 import Poll from '../poll/poll';
+import './content.css';
 
-
-const pollData = {
+const pollData = [{
     title: 'Favorite Color',
     votingType: 'single',
     options: ['Red', 'Blue', 'Green'],
     votes: [0, 0, 0],
     usersVoted: [],
-  };
+  },
+  {  title: 'Favorite Color',
+    votingType: 'single',
+    options: ['Red', 'Blue', 'Green'],
+    votes: [0, 0, 0],
+    usersVoted: [],
+  },
+  {  
+    title: 'Favorite Color',
+    votingType: 'single',
+    options: ['Red', 'Blue', 'Green'],
+    votes: [0, 0, 0],
+    usersVoted: [],
+  }
+];
 
 
 
 const Content = () => {
   return (
     <div>
-      <Poll {...pollData}/>
+      {
+      pollData.map((poll, index) => {
+        if (index%2==0 && index+1 < pollData.length) {
+          return (
+            <div class="rowPoll" key={index}>
+              <div class="colPoll">
+                <Poll {...poll} />
+              </div>
+              <div class="colPoll">
+                <Poll {...pollData[index+1]} />
+              </div>
+
+            </div>
+          );
+        }
+        else if(index%2==0){
+          return (
+            <div class="rowPoll" key={index}>
+            <div class="colPoll">
+              <Poll {...poll} />
+            </div>
+          </div>
+          );
+      }
+      })}
+
     </div>
   );
+  
 };
 
 export default Content;
